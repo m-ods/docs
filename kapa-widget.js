a222639db25a6623c494b93a3c538846ca2178c8
Martin Schweiger <martinschweigerod@gmail.com>
2025-01-23 06:35:56 +0800

    fix broken links

---
+++ kapa-widget.js
@@ -0,0 +1,8 @@
+const script = document.createElement("script");
+script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+script.async = true;
+script.setAttribute("data-website-id", "XXX-XXX-XXX");
+script.setAttribute("data-project-name", "PROJECT_NAME");
+script.setAttribute("data-project-color", "#HEX_COLOR_CODE");
+script.setAttribute("data-project-logo", "https://LINK_TO_LOGO.com/logo.png");
+document.head.appendChild(script);
